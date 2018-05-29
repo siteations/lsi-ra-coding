39fcc73b24b51cc887c5f78eb4ae4176dbbd8ec0
siteations <studer@siteations.com>
2018-05-29 16:08:02 -0400

    paper ready to start

---
+++ week-2/05_T/webpack-paper-image/src/filter.js
@@ -0,0 +1,27 @@
+import {p} from './index.js';
+import {curSize} from './pixelate.js';
+
+	/*
+//-----------
+
+4) create 3 filter functions, adapted from your pixel work that alter the color of the image - explore the color object attributes and consider your options
+
+	one can work with simple amplifications... like saturation
+	one should alter the rgb values of image
+	one should use a conditional logic to explore things like thresholds and conditional color replacement
+
+*/
+
+export const filter =function(image,size,type, val){
+
+}
+
+
+export const levels = function(image,size, colObj){
+
+}
+
+export const thresholds = function(image,size, level){
+
+
+}
